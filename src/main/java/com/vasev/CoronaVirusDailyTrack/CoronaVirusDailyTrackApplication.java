7888be187ce6655de90bb432ff8ad0d94da0ba87
GeorgiVasev <gvasevg@gmail.com>
2020-04-23 13:38:54 +0300

    Loading project plus dependecies

---
+++ src/main/java/com/vasev/CoronaVirusDailyTrack/CoronaVirusDailyTrackApplication.java
@@ -0,0 +1,13 @@
+package com.vasev.CoronaVirusDailyTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaVirusDailyTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaVirusDailyTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vasev/CoronaVirusDailyTrack/CoronaVirusDailyTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasev.CoronaVirusDailyTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaVirusDailyTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
